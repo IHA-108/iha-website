18d9e6c39991f2d9e290bf16e54e365e0c1d3c1a
IHA-108 <indianhempassociation@gmail.com>
2025-10-16 02:59:46 +0530

    Update robots.txt.js

--- pages/robots.txt.js
+++ pages/robots.txt.js
@@ -1,1 +1,16 @@
+export async function getServerSideProps({ res }) {
+  const content = `User-agent: *
+Allow: /
+
+Sitemap: https://www.indianhempassociation.com/sitemap.xml
+`;
+
+  res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+  res.write(content);
+  res.end();
+
+  return { props: {} };
+}
+
+export default function RobotsTxt() { return null; }
 
